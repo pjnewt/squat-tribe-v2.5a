7947163f1b9316d05743f084b4c1d49e7d74190c
pjnewt <pjnewt58@gmail.com>
2026-04-03 10:52:00 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,17 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("squat-tribe-v25").then(cache => {
+    caches.open("squat-tribe-v26a").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
         "./styles.css",
         "./app.js",
-        "./manifest.json"
+        "./manifest.json",
+        "./SS Back squat.png",
+        "./SS Bulgarian squat.png",
+        "./SS Front squat.png",
+        "./SS Side step.png",
+        "./SS Sumo squat.png"
       ]);
     })
   );
